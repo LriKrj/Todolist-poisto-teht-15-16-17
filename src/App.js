1968b21902b7e83dcf550743794bcb150132a5ca
LriKrj <lauri.karjalainen3@hotmail.com>
2022-03-06 13:38:38 +0200

    Tehtävä valmis

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [desc, setDesc] = useState({ desc: "", date: "" });
+  const [todos, setTodos] = useState([]);
+
+  const inputChanged = (event) => {
+    setDesc({ ...desc, [event.target.name]: event.target.value });
+  };
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, desc]);
+  };
+
+  const deleteClicked = (index) => {
+    setTodos(todos.filter((todo, i) => i !== index));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={addTodo}>
+        <label>Description:</label>
+        <input
+          type="text"
+          name="desc"
+          value={desc.desc}
+          onChange={inputChanged}
+        />
+        <label>Date:</label>
+        <input
+          type="text"
+          name="date"
+          value={desc.date}
+          onChange={inputChanged}
+        ></input>
+        <input type="submit" value="Add" />
+      </form>
+      <table>
+        <tr>
+          <th>Date</th>
+          <th>Description</th>
+        </tr>
+
+        <tbody>
+          {todos.map((todo, index) => (
+            <tr key={index}>
+              <td>{todo.date}</td>
+              <td>{todo.desc}</td>
+              <td>
+                <button onClick={() => deleteClicked(index)}>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
